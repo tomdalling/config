5fa66f8310bfb0fb49fe8ac1d0697d6265c6c81f
Tom Dalling <tom@tomdalling.com>
2014-04-04 23:06:27 +1100

    Improving install.sh

--- install.sh
+++ install.sh
@@ -11,10 +11,13 @@ replace_with_sym() {
     src="$1"
     dest="../.$src"
 
+    echo "Installing ${src}..."
+
     # if it exists and it isn't a symlink
     if [[ -f "$dest" && ! -L "$dest" ]] ; then
         # append it with a ".local" extension
         cat "$dest" >> "$dest.local"
+        echo "    Moved ~/.$src to ~/.${src}.local"
     fi 
 
     # remove it if it exists
@@ -27,8 +30,15 @@ replace_with_sym() {
 cd "$(dirname "$0")"
 [[ "$(pwd)" == "$HOME/config" ]] || fail "Must be install in $HOME/config"
 
+echo "Updating to latest version..."
+git fetch && git rebase
+echo
+echo
+
 replace_with_sym bash_profile
 replace_with_sym bashrc
 replace_with_sym irbrc
 replace_with_sym gitconfig
 replace_with_sym gemrc
+
+echo "Done"
