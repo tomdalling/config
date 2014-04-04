d19567eb5488d55bd3bea601c1f8c86a7bca0c25
Tom Dalling <tom@tomdalling.com>
2014-04-04 11:36:55 +0000

    Install script

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+fail() {
+    echo $1
+    return 1
+}
+
+replace_with_sym() {
+    src="$1"
+    dest="../.$src"
+
+    # if it exists and it isn't a symlink
+    if [[ -f "$dest" && ! -L "$dest" ]] ; then
+        # append it with a ".local" extension
+        cat "$dest" >> "$dest.local"
+    fi 
+
+    # remove it if it exists
+    rm -f "$dest"
+
+    #symlink to config file
+    ln -s "config/$src" "$dest"
+}
+
+cd "$(dirname "$0")"
+[[ "$(pwd)" == "$HOME/config" ]] || fail "Must be install in $HOME/config"
+
+replace_with_sym bash_profile
+replace_with_sym bashrc
+replace_with_sym irbrc
+replace_with_sym gitconfig
