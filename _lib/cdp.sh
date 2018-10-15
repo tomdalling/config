ecb7f61816df5bf2fdc0af96383fd1cd2393d82a
Tom Dalling <tom@tomdalling.com>
2018-10-15 11:23:02 +1100

    Remove proj/_super from cdp path

--- _lib/cdp.sh
+++ _lib/cdp.sh
@@ -19,7 +19,7 @@ cdp() {
 cdp_FindProjectDirectories() {
     local NAME="$1"
     find -L ~/proj -depth 1 -maxdepth 1 -type d -iname "$NAME"
-    find -L ~/proj/_super -depth 1 -maxdepth 1 -type d -iname "$NAME"
+    #find -L ~/proj/_super -depth 1 -maxdepth 1 -type d -iname "$NAME"
 }
 
 cdp_AllProjectNames() {
