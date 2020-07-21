44036467bd2a65dbd45368c0323687a0a62a7fec
Tom Dalling <tom@tomdalling.com>
2020-07-21 23:51:42 +1000

    Clean up kindle highlights script

--- bin/kindle_highlights_to_todoist_csv.rb
+++ bin/kindle_highlights_to_todoist_csv.rb
@@ -3,185 +3,180 @@
 require 'pathname'
 require 'time'
 require 'csv'
-
-def help
-  puts <<~END_HELP
-    Usage: #{$0} <path_to_clippings_txt> [book_query] [renamed_book]
-
-      <path_to_clippings_txt>
-        The path the to "My Clippings.txt" file from the Kindle
-
-      [book_query]
-        Optional. If given, will only output highlights from books that include
-        the given string.
-
-      [renamed_book]
-        Optional. If given, output will use this as the name of the book.
-  END_HELP
-  abort
+require 'bundler/inline'
+gemfile do
+  gem 'dry-cli', '0.6.0'
 end
 
-INPUT_PATH = ARGV[0] && Pathname(ARGV[0])
-BOOK_QUERY = ARGV[1]
-BOOK_RENAME = ARGV[2]
-help unless INPUT_PATH && INPUT_PATH.file?
+DEFAULT_CLIPPINGS_PATH = "/Volumes/Kindle/documents/My Clippings.txt"
 
-META_REGEX = %r{
-  \A # start of string
-  -\sYour\sHighlight\son\spage\s(?<page>\d+)
-  \s\|\s
-  location\s(?<loc_start>\d+)-(?<loc_end>\d+)
-  \s\|\s
-  Added\son\s(?<added_on>.+)
-  \z # end of string
-}x
+class Command < Dry::CLI::Command
+  desc <<~END_DESC
+    Takes highlights from a Kindle 'My Clippings.txt' file outputs them in
+    Todoist-compatible CSV format.
+  END_DESC
 
-META_REGEX2 = %r{
-  \A # start of string
-  -\sYour\sHighlight\sat\slocation\s(?<loc_start>\d+)-(?<loc_end>\d+)
-  \s\|\s
-  Added\son\s(?<added_on>.+)
-  \z # end of string
-}x
+  example [
+    '--only "Shape Up" --source "Ryan Singer, Jason Fried (2019) _Shape Up_"'
+  ]
 
-def main
-  clippings = clippings_from(INPUT_PATH)
-  write_clippings_as_csv(clippings)
-end
+  option :only, desc: "Restrict output to highlights from books that include this string"
+  option :source, desc: "Replace the book name with this in the output"
+  argument :path,
+    default: DEFAULT_CLIPPINGS_PATH,
+    desc: "The path to 'My Clippings.txt' take from a Kindle. Default: #{DEFAULT_CLIPPINGS_PATH}"
 
-def clippings_from(path)
-  INPUT_PATH.read(encoding: 'bom|utf-8')
-    .gsub(/\r\n?/, "\n")
-    .split(/^=+$/)
-    .map(&:strip)
-    .select { _1.include?("- Your Highlight") }
-    .map { parse_clipping(_1) }
-    .select { clipping_has_valid_book?(_1) }
-    .map { rename_book(_1) }
-end
+  HIGHLIGHT_PREFIX = "- Your Highlight"
 
-def parse_clipping(clipping)
-  book, meta, *quote = clipping.lines
-  meta_match = meta.strip.then { _1.match(META_REGEX) || _1.match(META_REGEX2) }
-  unless meta_match
-    raise "Can't parse clipping meta info: #{meta.strip}"
+  def call(path:, only: nil, source: nil)
+    clippings = clippings_from(path)
+      .select { clipping_has_valid_book?(_1, only) }
+      .map { rename_book(_1, source) }
+    write_clippings_as_csv(clippings)
   end
 
-  captures = meta_match
-    .named_captures
-    .map { [_1.to_sym, _2] }
-    .to_h || {}
+  private
 
-  xform_key(captures, :page) { Integer(_1) }
-  xform_key(captures, :loc_start) { Integer(_1) }
-  xform_key(captures, :loc_end) { Integer(_1) }
-  xform_key(captures, :added_on) { Time.parse(_1) }
+    def clippings_from(path)
+      File.read(path, encoding: 'bom|utf-8')
+        .gsub("\uFEFF", '') # wtf is a BOM doing in the middle of the file?
+        .gsub(/\r\n?/, "\n")
+        .split(/^=+$/)
+        .map(&:strip)
+        .select { _1.include?(HIGHLIGHT_PREFIX) }
+        .map { parse_clipping(_1) }
+    end
 
-  {
-    book: book.strip,
-    quote: quote.join.strip,
-    **captures,
-  }
-end
+    def parse_clipping(clipping)
+      book, meta_line, *quote = clipping.lines
+      meta = parse_meta_line(meta_line)
 
-def clipping_has_valid_book?(clipping)
-  if BOOK_QUERY
-    clipping.fetch(:book).include?(BOOK_QUERY)
-  else
-    true
-  end
-end
+      {
+        book: book.strip,
+        quote: quote.join.strip,
+        **meta,
+      }
+    end
 
-def rename_book(clipping)
-  if BOOK_RENAME
-    clipping.merge(book: BOOK_RENAME)
-  else
-    clipping
-  end
-end
+    def parse_meta_line(meta_line)
+      parts = meta_line.delete_prefix(HIGHLIGHT_PREFIX).split('|').map(&:strip)
+      parts.map do |p|
+        case p
+        when /\Aon page (\d+)(-\d+)?\z/
+          { page: Integer($1) }
+        when /\A(at )?location (\d+)-(\d+)\z/
+          { loc_start: Integer($2), loc_end: Integer($3) }
+        when /\AAdded on (.*)\z/
+          { added_on: Time.parse($1) }
+        else
+          raise "Unhandled meta part: #{p.inspect}"
+        end
+      end.reduce({}, :merge)
+    end
 
-def xform_key(hash, key)
-  value = hash[key]
-  if value
-    hash[key] = yield(value)
-  end
-  nil
-end
+    def clipping_has_valid_book?(clipping, only)
+      if only
+        clipping.fetch(:book).include?(only)
+      else
+        true
+      end
+    end
 
-def trunc(str)
-  max_len = 60
-  elip = "\u2026"
-  if str.length > max_len
-    str.slice(0, max_len - elip.length) + elip
-  else
-    str
-  end
-end
+    def rename_book(clipping, new_name)
+      if new_name
+        clipping.merge(book: new_name)
+      else
+        clipping
+      end
+    end
 
-def word_wrap(text, max_len:, prefix: '', suffix: '')
-  words = text.split
-  lines = []
-  max_inner_len = max_len - prefix.length - suffix.length
+    def xform_key(hash, key)
+      value = hash[key]
+      if value
+        hash[key] = yield(value)
+      end
+      nil
+    end
 
-  until words.empty?
-    next_word = words.shift
-    added_to_last = (lines.last || '') + ' ' + next_word
-    if !lines.empty? && added_to_last.length <= max_inner_len
-      lines[-1] = added_to_last
-    else
+    def trunc(str)
+      max_len = 60
+      elip = "\u2026"
+      if str.length > max_len
+        str.slice(0, max_len - elip.length) + elip
+      else
+        str
+      end
+    end
+
+    def word_wrap(text, max_len:, prefix: '', suffix: '')
+      words = text.split
+      lines = []
+      max_inner_len = max_len - prefix.length - suffix.length
+
+      until words.empty?
+        next_word = words.shift
+        added_to_last = (lines.last || '') + ' ' + next_word
+        if !lines.empty? && added_to_last.length <= max_inner_len
+          lines[-1] = added_to_last
+        else
+          lines[-1] += suffix unless lines.empty?
+          lines << prefix + next_word # new line
+        end
+      end
+
       lines[-1] += suffix unless lines.empty?
-      lines << prefix + next_word # new line
+      lines.join("\n")
     end
-  end
 
-  lines[-1] += suffix unless lines.empty?
-  lines.join("\n")
-end
+    def row_for_full_clipping(c)
+      location = c[:page] ? "page #{c[:page]}" : "location #{c.fetch(:loc_start)}"
+      block_quote = word_wrap(c.fetch(:quote), max_len: 80, prefix: '> ')
+      citation = [
+        "#{c.fetch(:book)} #{location}",
+        "Accessed: #{c.fetch(:added_on).iso8601}",
+      ].join("\n")
 
-def row_for_full_clipping(c)
-  block_quote = word_wrap(c.fetch(:quote), max_len: 80, prefix: '> ')
-  citation = [
-    "Book: " + c.fetch(:book),
-    c[:page] ? "Page: #{c[:page]}" : "Location: #{c.fetch(:loc_start)}-#{c.fetch(:loc_end)}",
-    "Accessed: #{c.fetch(:added_on).iso8601}",
-  ].join("\n")
-
-  {
-    TYPE: 'note',
-    CONTENT: block_quote + "\n\n" + citation,
-  }
-end
-
-def row_for_short_clipping(c)
-  {
-    TYPE: 'task',
-    CONTENT: 'Quote: ' + trunc(c.fetch(:quote)),
-    INDENT: 2,
-    PRIORITY: '4',
-  }
-end
-
-def row_for_book(book)
-  {
-    TYPE: 'task',
-    CONTENT: "Highlights for book: #{book}",
-    INDENT: 1,
-    PRIORITY: '4',
-  }
-end
-
-def write_clippings_as_csv(all_clippings)
-  headings = [:TYPE, :INDENT, :PRIORITY, :CONTENT]
-  csv = CSV.new(STDOUT)
-
-  csv << headings
-  all_clippings.group_by{ _1.fetch(:book) }.each do |book, book_clippings|
-    csv << row_for_book(book).values_at(*headings)
-    book_clippings.each do |c|
-      csv << row_for_short_clipping(c).values_at(*headings)
-      csv << row_for_full_clipping(c).values_at(*headings)
+      {
+        TYPE: 'note',
+        CONTENT: block_quote + "\n\n" + citation,
+      }
     end
-  end
+
+    def row_for_short_clipping(c)
+      {
+        TYPE: 'task',
+        CONTENT: 'Quote: ' + trunc(c.fetch(:quote)),
+        INDENT: 2,
+        PRIORITY: '4',
+      }
+    end
+
+    def row_for_book(book)
+      {
+        TYPE: 'task',
+        CONTENT: "Highlights for book: #{book}",
+        INDENT: 1,
+        PRIORITY: '4',
+      }
+    end
+
+    def write_clippings_as_csv(all_clippings)
+      headings = [:TYPE, :INDENT, :PRIORITY, :CONTENT]
+      csv = CSV.new(STDOUT)
+
+      csv << headings
+      all_clippings.group_by{ _1.fetch(:book) }.each do |book, book_clippings|
+        csv << row_for_book(book).values_at(*headings)
+        book_clippings.each do |c|
+          csv << row_for_short_clipping(c).values_at(*headings)
+          csv << row_for_full_clipping(c).values_at(*headings)
+        end
+      end
+    end
 end
 
-main
+
+if ARGV.empty? && !File.exist?(DEFAULT_CLIPPINGS_PATH)
+  ARGV << '--help'
+end
+Dry::CLI.new(Command).call
