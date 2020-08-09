40938770f46bc541679b87a9501f7c8fcc5a6fb8
Tom Dalling <tom@tomdalling.com>
2020-08-09 11:53:49 +1000

    bash: fix homebrew bash completion

--- _lib/brew_bash_completion.sh
+++ _lib/brew_bash_completion.sh
@@ -1,12 +1,5 @@
 if type brew &>/dev/null; then
-  for COMPLETION in $(brew --prefix)/etc/bash_completion.d/*
-  do
-    [[ -f $COMPLETION ]] && source "$COMPLETION"
-  done
-  if [[ -f $(brew --prefix)/etc/profile.d/bash_completion.sh ]];
-  then
-    source "$(brew --prefix)/etc/profile.d/bash_completion.sh"
-  fi
+  source_if_exists "$(brew --prefix)/etc/profile.d/bash_completion.sh"
 fi
 
 __git_complete g _git
