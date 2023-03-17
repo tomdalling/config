61f65282902c29b89cf0529945547cb34d6b8c23
Tom Dalling <tom@tomdalling.com>
2023-03-17 11:36:06 +1100

    Safely source project-local vimrcs

--- nvim/init.lua
+++ nvim/init.lua
@@ -300,6 +300,11 @@ vim.cmd [[
   augroup END
 ]]
 
+-- load vimrc files (from within .git/ dirs, for added safety)
+if vim.fn.filereadable('.git/vimrc') ~= 0 then
+  vim.cmd [[ source .git/vimrc ]]
+end
+
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
