eae4f0b13c14eb0ed6995244001ea3b4a7cbe9cf
Tom Dalling <tom@tomdalling.com>
2023-03-18 21:09:42 +1100

    Fix deprecation warning
    annoying

--- nvim/init.lua
+++ nvim/init.lua
@@ -360,7 +360,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig = require('lspconfig')
 lspconfig.solargraph.setup {
   capabilities = capabilities,
