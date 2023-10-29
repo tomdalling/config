68ba6046360249fd4613a0acd06f238c20289348
Tom Dalling <tom@tomdalling.com>
2023-10-29 17:14:17 +1100

    Cleaning/restructuring

--- README.md
+++ README.md
@@ -12,16 +12,3 @@ Neovim plugins:
 
     nvim
     :PlugInstall
-
-Old vim plugins:
-
-    git submodule init
-    git submodule update
-
-## Adding Vim Plugin Submodules
-
-Use the https github urls, so the repos can be cloned without credentials.
-
-    cd ~/config/vim/bundle
-    git submodule add https://github.com/tpope/vim-rails.git
-
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 fail() {
     echo $1
@@ -8,37 +8,37 @@ fail() {
 }
 
 replace_with_sym() {
-    src="$1"
-    dest="../.$src"
+    name="$1"
+    path="$2$1"
 
-    echo "Installing ${src}..."
+    dest="../.$name"
+
+    echo "Installing ${name}..."
 
     # if it exists and it isn't a symlink
     if [[ -f "$dest" && ! -L "$dest" ]] ; then
         # append it with a ".local" extension
         cat "$dest" >> "$dest.local"
-        echo "    Moved ~/.$src to ~/.${src}.local"
+        echo "    Moved ~/.$name to ~/.${name}.local"
     fi
 
     # remove it if it exists
     rm -Rf "$dest"
 
     #symlink to config file
-    ln -s "config/$src" "$dest"
+    ln -s "config/$path" "$dest"
 }
 
 cd "$(dirname "$0")"
 [[ "$(pwd)" == "$HOME/config" ]] || fail "Must be installed in $HOME/config"
 
-replace_with_sym bash_profile
-replace_with_sym bashrc
-replace_with_sym inputrc
-replace_with_sym irbrc
-replace_with_sym gitconfig
-replace_with_sym gemrc
-replace_with_sym vimrc
-replace_with_sym pryrc
-replace_with_sym byebugrc
-replace_with_sym vim
+replace_with_sym bash_profile bash/
+replace_with_sym bashrc bash/
+replace_with_sym inputrc ''
+replace_with_sym gitconfig git/
+replace_with_sym irbrc ruby/
+replace_with_sym gemrc ruby/
+replace_with_sym pryrc ruby/
+replace_with_sym byebugrc ruby/
 
 echo "Done"
